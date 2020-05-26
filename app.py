6a63fea3f8a0d203fb01aa6a45d1683612a88b51
Gonzalo Aldemar Mejía Zarate <gonzalo.mejia@ieb.local>
2020-05-26 00:59:38 -0500

    rutas ok

--- app.py
+++ app.py
@@ -9,10 +9,31 @@ from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     response = jsonify
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/download', methods=['GET'])
+def download():
+    response = jsonify
+    return render_template('download.html')
+
+@app.route('/upload', methods=['GET'])
+def upload():
+    response = jsonify
+    return render_template('upload.html')
+
+@app.route('/api', methods=['GET'])
+def api():
+    response = jsonify
+    return render_template('api.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    response = jsonify
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000, debug=True)
